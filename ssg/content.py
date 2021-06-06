ec553864bd798af3181b2f0ce1ca04de68f9bbb8
sreeja <govardhanasrija@gmail.com>
2021-06-06 19:16:59 +0530

    my work

--- ssg/content.py
+++ ssg/content.py
@@ -9,10 +9,10 @@ class Content(Mapping):
 
 
 @classmethod
-    def load(cls, string):
-        _, fm, content = cls.__regex.split(string, 2)
-        metadata = load(fm, Loader=FullLoader)
-        return cls(metadata, content)
+def load(cls, string):
+     _, fm, content = cls.__regex.split(string, 2)
+    metadata = load(fm, Loader=FullLoader)
+    return cls(metadata, content)
 
 def __init__(self, metadata, content):
         self.data = metadata
